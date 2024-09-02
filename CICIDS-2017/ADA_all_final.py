b6e11e988f00a50efc346ef97ef73b6538ad5de5
ogarreche <55901425+ogarreche@users.noreply.github.com>
2024-09-01 20:44:09 -0400

    Update ADA_all_final.py

--- CICIDS-2017/ADA_all_final.py
+++ CICIDS-2017/ADA_all_final.py
@@ -42,6 +42,9 @@ print('')
 
 req_cols = [' Destination Port',' Flow Duration',' Total Fwd Packets',' Total Backward Packets','Total Length of Fwd Packets',' Total Length of Bwd Packets',' Fwd Packet Length Max',' Fwd Packet Length Min',' Fwd Packet Length Mean',' Fwd Packet Length Std','Bwd Packet Length Max',' Bwd Packet Length Min',' Bwd Packet Length Mean',' Bwd Packet Length Std','Flow Bytes/s',' Flow Packets/s',' Flow IAT Mean',' Flow IAT Std',' Flow IAT Max',' Flow IAT Min','Fwd IAT Total',' Fwd IAT Mean',' Fwd IAT Std',' Fwd IAT Max',' Fwd IAT Min','Bwd IAT Total',' Bwd IAT Mean',' Bwd IAT Std',' Bwd IAT Max',' Bwd IAT Min','Fwd PSH Flags',' Bwd PSH Flags',' Fwd URG Flags',' Bwd URG Flags',' Fwd Header Length',' Bwd Header Length','Fwd Packets/s',' Bwd Packets/s',' Min Packet Length',' Max Packet Length',' Packet Length Mean',' Packet Length Std',' Packet Length Variance','FIN Flag Count',' SYN Flag Count',' RST Flag Count',' PSH Flag Count',' ACK Flag Count',' URG Flag Count',' CWE Flag Count',' ECE Flag Count',' Down/Up Ratio',' Average Packet Size',' Avg Fwd Segment Size',' Avg Bwd Segment Size',' Fwd Header Length','Fwd Avg Bytes/Bulk',' Fwd Avg Packets/Bulk',' Fwd Avg Bulk Rate',' Bwd Avg Bytes/Bulk',' Bwd Avg Packets/Bulk','Bwd Avg Bulk Rate','Subflow Fwd Packets',' Subflow Fwd Bytes',' Subflow Bwd Packets',' Subflow Bwd Bytes','Init_Win_bytes_forward',' Init_Win_bytes_backward',' act_data_pkt_fwd',' min_seg_size_forward','Active Mean',' Active Std',' Active Max',' Active Min','Idle Mean',' Idle Std',' Idle Max',' Idle Min',' Label']
 
+
+
+
 #----------------------------------------------------------------------------------------------------------
 #Defining metric functions
 def ACC(TP,TN,FP,FN):
@@ -117,7 +120,24 @@ print('')
 df_max_scaled = df.copy()
 print(df_max_scaled)
 
-y = df_max_scaled[' Label'].replace({'DDoS':'Dos/Ddos','DoS GoldenEye': 'Dos/Ddos', 'DoS Hulk': 'Dos/Ddos', 'DoS Slowhttptest': 'Dos/Ddos', 'DoS slowloris': 'Dos/Ddos', 'Heartbleed': 'Dos/Ddos','FTP-Patator': 'Brute Force', 'SSH-Patator': 'Brute Force','Web Attack - Brute Force': 'Web Attack', 'Web Attack - Sql Injection': 'Web Attack', 'Web Attack - XSS': 'Web Attack'})
+# y = df_max_scaled[' Label'].replace({'DDoS':'Dos/Ddos','DoS GoldenEye': 'Dos/Ddos', 'DoS Hulk': 'Dos/Ddos', 'DoS Slowhttptest': 'Dos/Ddos', 'DoS slowloris': 'Dos/Ddos', 'Heartbleed': 'Dos/Ddos','FTP-Patator': 'Brute Force', 'SSH-Patator': 'Brute Force','Web Attack - Brute Force': 'Web Attack', 'Web Attack - Sql Injection': 'Web Attack', 'Web Attack - XSS': 'Web Attack'})
+
+y = df_max_scaled['Label'].replace({'DoS GoldenEye': 'Dos/Ddos', 
+                                    'DoS Hulk': 'Dos/Ddos', 
+                                    'DoS Slowhttptest': 'Dos/Ddos', 
+                                    'DoS slowloris': 'Dos/Ddos', 
+                                    'Heartbleed': 'Dos/Ddos', 
+                                    'DDoS': 'Dos/Ddos',
+                                    'FTP-Patator': 'Brute Force', 
+                                    'SSH-Patator': 'Brute Force',
+                                    'Web Attack - Brute Force': 'Web Attack', 
+                                    'Web Attack - Sql Injection': 'Web Attack',
+                                    'Web Attack - XSS': 'Web Attack',                                
+                                    'Web Attack   XSS': 'Web Attack',
+                                    'Web Attack   Sql Injection': 'Web Attack',
+                                    'Web Attack   Brute Force': 'Web Attack'                                   
+                                   })
+
 
 df_max_scaled.pop(' Label')
 
